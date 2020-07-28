c6fc411a280d54f98daed0bcd8f76fe350796217
Natsuki <nkondo3@gmail.com>
2020-07-28 14:43:47 +0900

    Updates

---
+++ precache-manifest.c222bc6effee89ddf56838e8a86c190e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f08f8f39ec2a8dad70ffeb8f87fb6fa",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "31474428333402852350",
+    "url": "/react-todolist/static/css/main.2a1fdca6.chunk.css"
+  },
+  {
+    "revision": "2c911e193239d1d39dbe",
+    "url": "/react-todolist/static/js/2.81c53a68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.81c53a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31474428333402852350",
+    "url": "/react-todolist/static/js/main.f986e93b.chunk.js"
+  },
+  {
+    "revision": "bdb1eb0d6159838c9f4b",
+    "url": "/react-todolist/static/js/runtime-main.6228bffe.js"
+  }
+]);
