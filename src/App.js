c9053e559dc971983f6ed6e18a8d6cbdc3dc9f70
Natsuki <nkondo3@gmail.com>
2020-09-08 00:03:51 +0900

    fixing edit

--- src/App.js
+++ src/App.js
@@ -4,54 +4,63 @@ import React, { Component } from 'react';
 class TodoApp extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      id: 1,
+      id: 0,
       newItem: "",
-      list: []
+      list: [],
+      isDone: false,
     }
   }
 
-  updateInput(key, value) {
-    this.setState({
-      [key]: value
-    });
-  }
-
   addItem = () => {
-    // if (this.state.newItem.trim() === '') {
-    //   return;
-    // }
-
+    if (this.state.newItem.trim() === '') {
+      return
+    }
     const id = this.state.id + 1;
     const item = {
       id,
-      value: this.state.newItem
-    };
-
-    const list = [...this.state.list, item];
+      label: this.state.newItem
+    }
+    const list = [...this.state.list, item]
 
     this.setState({
-      id,
-      newItem: "",
-      list
-    });
+      list,
+      newItem: ""
+    })
+  }
+
+  onChange = (e) => {
+    this.setState({
+      newItem: e.target.value
+    })
   }
 
   deleteItem(id) {
+    debugger
     const list = [...this.state.list];
-    const updatedList = list.filter(item => item.id !== id);
+    const updatedList = list.filter(item => item.id !== id)
 
-    this.setState({ list: updatedList })
+    this.setState({
+      list: updatedList
+    })
   }
 
 
-  checkTodo() {
+  editItem(id) {
+    debugger
+    const list = [...this.state.list]
+    const updatedList = list.filter(item => item.id !== id)
+    const selectedItem = this.state.list.find(item => item.id === id)
 
+    this.setState({
+      editItem: true,
+      list: updatedList,
+      newItem: selectedItem,
+    })
   }
+
 
   render() {
-    debugger
     return (
       <div className="TodoApp">
         <h1>Add an Item...</h1>
@@ -60,28 +69,41 @@ class TodoApp extends Component {
           type="text"
           placeholder="Type list here.."
           value={this.state.newItem}
-          onChange={e => this.updateInput("newItem", e.target.value)}
+          onChange={this.onChange}
         />
-        <button 
+        <button
           className="add"
-          disabled={this.state.newItem.trim().length === 0}
+          // disabled={this.state.newItem.trim().length === 0}
           onClick={this.addItem}
         >
-          Add
+          {this.selectedItem ? "Done" : "Add"}
         </button>
         <br />
         <ul>
           {this.state.list.map(item => {
             return (
-              <li key={item.id}>
-                <input type="checkbox" className="checkbox" />
-                {item.value}
+              <li key={item.label} >
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  // checked={  }
+                />
+                <span className={this.isDone ? 'done' : ''}>
+                  {item.label}
+                </span>
+
+                <button className="edit"
+                  onClick={() => this.editItem(item.id)}
+                >
+                  <i className="fa fa-pen"></i>
+                </button>
+
                 <button className="del"
                   onClick={() => this.deleteItem(item.id)}
                 >
-                  X
-               </button>
-              
+                  <i className="fa fa-trash"></i>
+                </button>
+
               </li>
             )
           })}
